76fbe999fbdb470ae94543768d56e83fa4fc3764
Sovietsupporter69 <84580170+Sovietsupporter69@users.noreply.github.com>
2024-10-03 22:42:48 +0100

    Got the trasition top bar working
    Also did a re-color, next thing to do is find a temp image for the main page so I can work on the layout

--- Included/NavigationBar.php
+++ Included/NavigationBar.php
@@ -1,9 +1,19 @@
+
 <div id="NavContainer">
-    <div id="NavBarContainer">
-        <div id="NavDropContainer"><img id="dropimage" src="Images/Dropdown.png" alt="menu button"></div>
-        <div id="NavFill"></div>
+    <input type="checkbox" id="BarMover"></input>
+    <div id="BurgerContainer">
+        <svg id="Burger" width="60" height="56" viewBox="0 0 60 60">
+            <line x1="12" y1="16" x2="48" y2="16" class="BurgerLine"></line>
+            <line x1="12" y1="31" x2="48" y2="31" class="BurgerLine"></line>
+            <line x1="12" y1="46" x2="48" y2="46" class="BurgerLine"></line>
+        </svg>
     </div>
     <ul id="NavList">
+        <li class="NavSpacer">
+            <div class="NavSpacerFirst"></div>
+            <div class="NavSpacerMid"></div>
+            <div></div>
+        </li>
         <a href="Find_Us"><li class="NavBox">About me</li></a>
         <li class="NavSpacer">
             <div class="NavSpacerFirst"></div>
